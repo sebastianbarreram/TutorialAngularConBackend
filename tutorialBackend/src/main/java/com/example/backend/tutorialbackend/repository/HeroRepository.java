5ae4c932f60647dd1df017001be007a81c67aab8
unknown <sbarrera.96@hotmail.com>
2022-07-16 22:36:31 -0500

    Init folders backend and frontend

---
+++ tutorialBackend/src/main/java/com/example/backend/tutorialbackend/repository/HeroRepository.java
@@ -0,0 +1,7 @@
+package com.example.backend.tutorialbackend.repository;
+
+import com.example.backend.tutorialbackend.model.Hero;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface HeroRepository extends ReactiveMongoRepository<Hero, String> {
+}
